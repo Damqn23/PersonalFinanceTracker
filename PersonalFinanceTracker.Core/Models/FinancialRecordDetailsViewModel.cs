42c9109d653101590be0c226d1a27b64bbc016be
Damqn23 <rauga2005@gmail.com>
2024-07-22 01:21:47 +0300

    Models added

---
+++ PersonalFinanceTracker.Core/Models/FinancialRecordDetailsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using PersonalFinanceTracker.Infrastructure.Data.Models;
+
+namespace PersonalFinanceTracker.Core.Models
+{
+    public class FinancialRecordDetailsViewModel
+    {
+        public FinancialRecord Record { get; set; } = new FinancialRecord();
+    }
+}
---
+++ PersonalFinanceTracker.Core/Models/FinancialRecordIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using PersonalFinanceTracker.Infrastructure.Data.Models;
+using System.Collections.Generic;
+
+namespace PersonalFinanceTracker.Core.Models
+{
+    public class FinancialRecordIndexViewModel
+    {
+        public List<FinancialRecord> Records { get; set; } = new List<FinancialRecord>();
+    }
+}
